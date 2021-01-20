93f74e80396f3d4f09cfdf5eb2f6aa32366e0979
Glenn Hansen <hansenglenneirik@gmail.com>
2021-01-20 12:33:30 +0100

    Finished exercise 3

--- class3_exercises/main.js
+++ class3_exercises/main.js
@@ -1,18 +1,28 @@
 // EXERCISE 2 //
 function fissBuzz(){
     for(let i = 1; i <= 100; i++){
-        if(i % 3 === 0){
-            console.log("Fizz");
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("Fizz Buzz");
         }else if(i % 5 === 0){
             console.log("Buzz");
-        }else if(i % 3 === 0 && i % 5 === 0){
-            console.log("Fizz Buzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz");
         }else {
             console.log(i);
         }
     }
 }
-
 fissBuzz();
 
 // EXERCISE 3 //
+function pattern(){
+    let myArray = [];
+    for(let i = 0; i < 5; i++){
+        myArray.push("*");
+        for(let x = 0; x < 1; x++){
+            myArray.push(" ");
+        }
+        console.log(myArray.toString().replace(/,/g, ""));
+    }
+}
+pattern();
